c4058ee62879f27a8bd613031ccfd686074b4c51
Bala1410 <46164904+Bala1410@users.noreply.github.com>
2019-01-08 18:26:10 +0530

    Create largestnum.py

---
+++ largestnum.py
@@ -0,0 +1,9 @@
+a=float(input("enter the num 1="))
+b=float(input("enter the num 2="))
+c=float(input("enter the num 3="))
+if((a>b) and (a>c)):
+    print("a is largest number")
+elif((b>a) and (b>c)):
+    print("b is largest number")
+else:
+    print("c is largest number")
