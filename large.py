ca9c9475b89ca58fd23d31b79f2d6b7e5c5f291c
Bala1410 <46164904+Bala1410@users.noreply.github.com>
2019-01-08 18:37:30 +0530

    Create large.py

---
+++ large.py
@@ -0,0 +1,9 @@
+a=float(input("enter the num 1="))
+b=float(input("enter the num 2="))
+c=float(input("enter the num 3="))
+if((a>b) and (a>c)):
+    print(a)
+elif((b>a) and (b>c)):
+    print(b)
+else:
+    print(c)
