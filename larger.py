d2c9836765888b13d2bde8a578127b55719459fc
Bala1410 <46164904+Bala1410@users.noreply.github.com>
2019-06-06 21:40:19 +0530

    Update larger.py

--- larger.py
+++ larger.py
@@ -1,9 +1,3 @@
-a=int(input("enter the num "))
-b=int(input("enter the num "))
-c=int(input("enter the num "))
-if((a>b) and (a>c)):
-    print(a)
-elif((b>a) and (b>c)):
-    print(b)
-else:
-    print(c)
+a=list(map(int,input().split()))
+a.sort()
+print(max(a))
