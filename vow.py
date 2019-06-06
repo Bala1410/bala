ef8be6b936e03df864813ca73d965130fc610c29
Bala1410 <46164904+Bala1410@users.noreply.github.com>
2019-06-06 11:38:13 +0530

    Add files via upload

---
+++ vow.py
@@ -0,0 +1,9 @@
+c=input()
+if((c>='a') and (c<='z')or(c>='A') and (c<='z')):
+    if((c=='a') or (c=='e') or (c=='i') or(c=='o')or(c=='u')) or ((c=='A') or (c=='E') or (c=='I')or(c=='O')or (c=='U')):
+          print("vowel")
+    else:
+          print("consonant")
+else:
+    print("invalid")
+     
